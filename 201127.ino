ea35fa409baf5ed9466608a13f2ebf1d31dc0b04
sj0020 <62295464+sj0020@users.noreply.github.com>
2020-11-29 16:31:19 +0900

    Add files via upload
    ec + servo + relay
    cds + led

--- 201127.ino
+++ 201127.ino
@@ -7,9 +7,11 @@
 DHT dht(DHTPIN, DHTTYPE);
 Servo servo;      //Servo 클래스로 servo객체 생성
 int soil = A2;
-int dcmt = 3;
+int dcmt = 7;
 int servoPin = 4;
 int angle = 70; // servo position in degrees 
+int cds = A1;
+int led = 6;
 
 unsigned int arr[6] = {0,}; //순서대로 0온도, 1습도, 2EC, 3미세먼지, 4CO₂, 5조도 센서
 
@@ -23,13 +25,11 @@ void setup() {
 void loop() {
   //#include "functions.h"
   dht22();
+  ec();
+  cdsled();
   
-  int cds = analogRead(A1);
-  arr[5] = cds;
-  Serial.print("cds값은:");
-  Serial.println(arr[5]);
 
-  void servomt();
+  delay(500);
 }
 
 
@@ -49,16 +49,15 @@ void dht22(){
     Serial.println(F("°C "));
   }
 
-void servomt(){
+void ec(){
   int value = analogRead(soil);
   arr[2] = value;
-  Serial.print("센서 값");
-  Serial.println(arr[2]);
+  Serial.print("ec센서 값");
+  Serial.println(arr[2]);  
   delay(500);
   
   if(value <=100){
-    digitalWrite(3, 1);
-   
+    digitalWrite(dcmt, 1);
       for(angle = 80; angle < 135; angle++) 
     { 
       servo.write(angle); 
@@ -83,7 +82,21 @@ void servomt(){
   }
   
   else if(value >600){
-    digitalWrite(3, 0);
+    digitalWrite(dcmt, 0);
+  }
+} 
+
+void cdsled(){
+  int cdsvalue = analogRead(cds);
+  arr[5] = cdsvalue;
+  Serial.print("cds값은:");
+  Serial.println(arr[5]);
+  if (cdsvalue < 500){
+    digitalWrite (led, 1);
+    }
+  else {
+    digitalWrite (led, 0);
+    }    
   }
 
-  }  
+    
